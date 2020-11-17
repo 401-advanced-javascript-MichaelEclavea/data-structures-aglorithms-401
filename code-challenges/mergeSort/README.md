c41f20e25ff544c618fec369c34d398665f86ced
Michael Eclavea <usermike671@gmail.com>
2020-11-17 13:48:13 -0800

    made functions, no tests

---
+++ code-challenges/mergeSort/README.md
@@ -0,0 +1,11 @@
+# Merge Sort
+
+- In order to divide the array in half, we need to figure out the middle.
+take array and divide by 2. 
+
+- This is where we will be dividing the array into left and right.
+slice the first index to mid. 
+slice the mid, which will equal the right side. 
+
+- Using recursion to combine the left and right
+merge left and right arrays into one. 
---
+++ code-challenges/mergeSort/merge-sort.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let array = [10, 15, 22, 5, 11, 6, 8];
+   
+function mergeSort (unsortedArray) {
+ if (unsortedArray.length <= 1) {
+   return unsortedArray;
+ }
+
+ const mid = Math.floor(unsortedArray.length / 2);
+ const left = unsortedArray.slice(0, mid);
+ const right = unsortedArray.slice(mid);
+ return merge(
+   mergeSort(left), mergeSort(right)
+ );
+}
+
+
+
+
+
+ function merge (left, right) {
+     let finalArray = [];
+     let leftIndex = 0;
+     let rightIndex = 0;
+
+     while (leftIndex < left.length && rightIndex < right.length) {
+       if (left[leftIndex] < right[rightIndex]) {
+         finalArray.push(left[leftIndex]);
+         leftIndex++;
+       } else {
+         finalArray.push(right[rightIndex]);
+         rightIndex++; 
+       }
+     }
+
+     return finalArray
+             .concat(left.slice(leftIndex))
+             .concat(right.slice(rightIndex));
+   }
+
+mergeSort(array);
